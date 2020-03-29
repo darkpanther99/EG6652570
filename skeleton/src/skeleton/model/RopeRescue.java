3719042d7b98d0afae437def9648af9f6dfa54b1
Botondar <m.botond7@gmail.com>
2020-03-29 18:08:15 +0200

    orElseThrow() -> orElseThrow(null)

--- skeleton/src/skeleton/model/RopeRescue.java
+++ skeleton/src/skeleton/model/RopeRescue.java
@@ -3,13 +3,16 @@ package skeleton.model;
 import static skeleton.Logger.*;
 
 import java.util.Collection;
+import java.util.function.Supplier;
 
 public class RopeRescue implements RescueStrategy {
     public void Rescue(Tile water, Tile land) {
         logMethodCall(this, water, land);
         Collection<Player> occ = water.getOccupants();
         if (occ.size() > 0) {
-            Player p = occ.stream().findFirst().orElseThrow();
+        	
+            Player p = occ.stream().findFirst().orElseThrow(null);
+            
             p.PlaceOn(land);
             // A szekvenciáról hiányzik, a water.Remove(p).
             // De úgy se tudom hívni mert privát, úgyhogy csalok.
