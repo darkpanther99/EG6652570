12c02fff0f16fde86686bcb1baa718c018f93e0d
Botondar <m.botond7@gmail.com>
2020-04-26 15:21:03 +0200

    jdoc

--- proto/src/proto/EquipCommandParser.java
+++ proto/src/proto/EquipCommandParser.java
@@ -2,14 +2,30 @@ package proto;
 
 import java.util.List;
 
+/**
+ * Szöveges argumentum listából EquipCommand-ot létrehozó osztály
+ */
 public class EquipCommandParser implements CommandParser {
+    /**
+     * A parancshoz tartozó kulcsszó
+     */
     private String keyword = "equip";
 
+    /**
+     * Visszaadja a parancshoz tartozó kulcsszót
+     * @return A kulcsszó. Mindig "equip"
+     */
     @Override
     public String getKeyword() {
         return keyword;
     }
 
+    /**
+     * EquipCommandot hoz létre az argumentumokból
+     * @param tokens az argumentumok. { "equip", "$i" }, i lehet "all" vagy az inventory egy indexe.
+     * @return EquipCommand parancs a megfelelő argumentumokkal
+     * @throws ProtoException helytelen bemenet esetén kivételt dob.
+     */
     @Override
     public Command parse(List<String> tokens) throws ProtoException {
         if(tokens.size() < 2 || !tokens.get(0).contentEquals(keyword)) {
--- proto/src/proto/ExamineCommand.java
+++ proto/src/proto/ExamineCommand.java
@@ -3,12 +3,24 @@ package proto;
 import proto.model.PolarExplorer;
 
 public class ExamineCommand implements Command {
+    /**
+     * Az szomszédos mező iránya, amit a sarkkutató vizsgál
+     */
     private int direction;
 
+    /**
+     * Konstruktor
+     * @param direction Az szomszédos mező iránya, amit a sarkkutató vizsgál
+     */
     public ExamineCommand(int direction) {
         this.direction = direction;
     }
 
+    /**
+     * Megvizsgálja a sarkkutató a szomszédos mezőt.
+     * @param state
+     * @throws ProtoException ha nincs játékos/nem sarkkutató van kiválasztva, kivételt dob.
+     */
     @Override
     public void execute(Proto state) throws ProtoException {
 
