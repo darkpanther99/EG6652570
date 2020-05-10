61134a8b9f3b74bdb5bb66d06f711f3930d6715b
Mark Konrad <konrad0816@gmail.com>
2020-05-11 01:12:49 +0200

    Reset, mert ez még nekem is ronda volt

--- grafikus/src/grafikus/mapgen/MapGen.java
+++ grafikus/src/grafikus/mapgen/MapGen.java
@@ -27,23 +27,15 @@ public class MapGen {
         for (int i = 0; i <= cols*rows-1; i++) tiles.add(new Tile());
 
         for (int x = 0; x < cols; x++) {
-            tiles.get(index(x,0)).setSnow(-1);
+            tiles.get(index(x,0)).setSnow(0);
             tiles.get(index(x,0)).setWeightLimit(0);
-            tiles.get(index(x,rows-1)).setSnow(-2);
+            tiles.get(index(x,rows-1)).setSnow(0);
             tiles.get(index(x,rows-1)).setWeightLimit(0);
-            if (x == 0) {
-                tiles.get(index(x,0)).setSnow(-5);
-                tiles.get(index(x,rows - 1)).setSnow(-8);
-            }
-            if (x == cols - 1) {
-                tiles.get(index(x,0)).setSnow(-6);
-                tiles.get(index(x,rows - 1)).setSnow(-7);
-            }
         }
-        for (int y = 1; y < rows - 1; y++) {
-            tiles.get(index(0,y)).setSnow(-3);
+        for (int y = 0; y < rows; y++) {
+            tiles.get(index(0,y)).setSnow(0);
             tiles.get(index(0,y)).setWeightLimit(0);
-            tiles.get(index(cols-1,y)).setSnow(-4);
+            tiles.get(index(cols-1,y)).setSnow(0);
             tiles.get(index(cols-1,y)).setWeightLimit(0);
         }
         //FIXME(Mark): Ez még itt nem működik, valami indexet elrontottam.
