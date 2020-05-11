e0f698ad70ebdf1e3a79a28137250ca2ecdaefbb
Botondar <m.botond7@gmail.com>
2020-05-11 15:00:48 +0200

    Game over dialog update.

--- grafikus/src/grafikus/MainMenu.java
+++ grafikus/src/grafikus/MainMenu.java
@@ -81,7 +81,7 @@ public class MainMenu extends JFrame implements WindowListener, ChangeListener, 
     @Override
     public void gameOver() {
         controller.dispatchEvent(new WindowEvent(controller, WindowEvent.WINDOW_CLOSING));
-        JOptionPane.showMessageDialog(this, "Game over!");
+        JOptionPane.showMessageDialog(this, "A player die, game over!");
     }
 
     @Override
