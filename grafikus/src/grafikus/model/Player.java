c6b0227b6543fb196484c42f35fa8e9b51ca267f
lantgabor <lant.gabor98@gmail.com>
2020-05-17 22:07:38 +0200

    ho alol item kiszopas fix

--- grafikus/src/grafikus/model/Player.java
+++ grafikus/src/grafikus/model/Player.java
@@ -120,7 +120,7 @@ public abstract class Player extends Entity {
      * Ezt a metódust a Controller hívja. A játékos felvesz egy tárgyat. 1 munkaegység
      */
     public void pickUp() {
-        if (energy > 0 || !(currentTile.getItem() instanceof Empty)) {
+        if (energy > 0 || !(currentTile.getItem() instanceof Empty) || (currentTile.getSnow() <= 0)) {
             decrementEnergy();
             Item item = currentTile.takeItem();
             addToInventory(item);
