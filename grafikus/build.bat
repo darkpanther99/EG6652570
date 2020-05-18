f41214830c60e84319fc5125de50e639c3c532a4
GlavitsBalazs <glavits.balazs@gmail.com>
2020-05-18 12:44:29 +0200

    buildscript update

--- grafikus/build.bat
+++ grafikus/build.bat
@@ -7,7 +7,7 @@ set outjar=%~dp0out\grafikus.jar
 if exist %classfolder% rd /S /Q %classfolder%
 if exist %outjar% del %outjar%
 set JAVA_HOME=C:\Program Files\Java\jdk-11.0.2
-"%JAVA_HOME%\bin\javac.exe" -encoding UTF-8 -d %classfolder% %src%\grafikus\*.java %src%\grafikus\model\*.java %src%\grafikus\maploader\*.java
+"%JAVA_HOME%\bin\javac.exe" -encoding UTF-8 -d %classfolder% %src%\grafikus\*.java %src%\grafikus\model\*.java
 xcopy %res%\* %classfolder% /s /i /q > nul
 "%JAVA_HOME%\bin\jar.exe" cfm %outjar% %src%\META-INF\MANIFEST.MF -C %classfolder% .
 endlocal
