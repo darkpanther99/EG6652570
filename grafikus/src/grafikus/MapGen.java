839356ba25f9754e587eb0b5d7c3dd92361c4a64
Mark Konrad <konrad0816@gmail.com>
2020-05-18 02:11:10 +0200

    Végtelen loop meggyilkolása, itemek száma függ a map méretétől

--- grafikus/src/grafikus/MapGen.java
+++ grafikus/src/grafikus/MapGen.java
@@ -8,38 +8,19 @@ import java.util.List;
 import java.util.Random;
 
 public class MapGen {
-    private static final boolean TESTING = false;
     private static Game game;
     private static int rows = 7;
     private static int cols = 10;
 
-    private MapGen() {
-
-    }
-
-    enum Direction {
-        NORTH(0),
-        EAST(1),
-        SOUTH(2),
-        WEST(3);
-
-        private final int value;
-
-        Direction(int value) {
-            this.value = value;
-        }
-
-        public int getValue() {
-            return value;
-        }
-    }
+    private MapGen() { }
 
     private static int index(int col, int row) {
         return cols * row + col;
     }
 
-    private static void generateTiles() {
+    private static boolean generateTiles() {
         List<Tile> tiles = game.getTiles();
+        tiles.clear();
         for (int i = 0; i <= cols * rows - 1; i++) {
             game.createTile(new Random().nextInt(5 + 1), new Random().nextInt(game.getPlayers().size() + 1 + 1) - 1);
             //game.createTile(0,1);
@@ -68,10 +49,10 @@ public class MapGen {
 
         tiles.get(index(1, 1)).setWeightLimit(999);
         tiles.get(index(1,1)).setChillWaterStrategy(new DryLand()); // NOTE(Mark): Volt egy bug, hogy elkezdtek meghalni a fiúk az első mezőn, ez azért volt, mert amikor legenerálódott, akkor még 0 volt a weight limit, ezért Sea ChillWaterStrategy volt.
-
+        return true;
     }
 
-    private static void generateItems() {
+    private static boolean generateItems() {
         List<Tile> tiles = game.getTiles();
         LinkedList<PartView> parts = new LinkedList<>(Arrays.asList(new PartView(ResourceManager.flareGun), new PartView(ResourceManager.flareLight), new PartView(ResourceManager.flare)));
         while (parts.size() > 0) {
@@ -84,12 +65,12 @@ public class MapGen {
             }
         }
         LinkedList<Item> items = new LinkedList<>();
-        int numShovels = 2;
-        int numBreakingShovels = 2;
-        int numRopes = 2;
-        int numFood = 5;
-        int numScuba = 2;
-        int numTentKit = 2;
+        int numShovels = (int)Math.ceil((rows-2)*(cols-2)/15.0);
+        int numBreakingShovels = (int)Math.ceil((rows-2)*(cols-2)/15.0);
+        int numRopes = (int)Math.ceil((rows-2)*(cols-2)/15.0);
+        int numFood = (int)Math.ceil((rows-2)*(cols-2)/8.0);
+        int numScuba = (int)Math.ceil((rows-2)*(cols-2)/15.0);
+        int numTentKit = (int)Math.ceil((rows-2)*(cols-2)/15.0);
         for (int i = 0; i < numShovels; i++) items.push(new Shovel());
         for (int i = 0; i < numBreakingShovels; i++) items.push(new BreakingShovel(3));
         for (int i = 0; i < numRopes; i++) items.push(new Rope());
@@ -106,52 +87,73 @@ public class MapGen {
                 tiles.get(index(x, y)).setItem(items.pop());
             }
         }
-
+        return true;
 
     }
 
-    private static void placeBears() {
+    private static boolean placeBears() {
         List<Tile> tiles = game.getTiles();
+        int tries = 0;
         for (PolarBear b : game.getBears()) {
             int done = 0;
             boolean badbadbear = false;
-            while (done < game.getBears().size()) {
+            while (done < game.getBears().size() && tries < rows*cols) {
                 int x = new Random().nextInt(cols - 2) + 1;
                 int y = new Random().nextInt(rows - 2) + 1;
                 if (tiles.get(index(x, y)).getWeightLimit() <= 0) {
+                    tries++;
                     continue;
                 }
                 for (Entity e : tiles.get(index(x, y)).getOccupants()) {
                     if (e instanceof Player) {
                         badbadbear = true;
+                        tries++;
                         break;
                     }
                 }
-                if (badbadbear) continue;
+                if (badbadbear) {
+                    tries++;
+                    continue;
+                }
 
                 if (b.getCurrentTile() != null) {
                     b.getCurrentTile().getOccupants().remove(b);
                 }
                 b.placeOn(tiles.get(index(x, y)));
                 done++;
+
             }
+            if (tries >= rows*cols-1) return false;
         }
+        return true;
     }
-    private static void placePlayers() {
+    private static boolean placePlayers() {
         List<Tile> tiles = game.getTiles();
+
         for (Player p : game.getPlayers()) {
+            if (p.getCurrentTile() != null) {
+                p.getCurrentTile().getOccupants().remove(p);
+            }
             p.placeOn(tiles.get(index(1, 1)));
         }
+        return true;
     }
 
     public static void generateMap(Game g, int r, int c) {
         rows = r;
         cols = c;
         game = g;
-
-        generateTiles();
-        generateItems();
-        placePlayers();
-        placeBears();
+        boolean done = false;
+        while (!done) {
+            generateTiles();
+            generateItems();
+            placePlayers();
+            while (!placeBears()) {
+                if (game.getBears().get(0).getCurrentTile() != null)
+                    game.getBears().get(0).getCurrentTile().getOccupants().remove(game.getBears().get(0));
+                game.getBears().remove(0);
+            }
+            done = true;
+        }
     }
 }
